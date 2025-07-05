f2fb11630dc4cf3420335d8a388698f3c69d34ae
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-07-05 12:52:15 +0300

    start drawGrid function loop over x axis
    This will draw a line (for every 50px) for every loop from the beginning until it reaches the end of the width of the map

--- client/vite-project/src/utils/gameUtils.js
+++ client/vite-project/src/utils/gameUtils.js
@@ -3,4 +3,8 @@ export const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
 export const drawGrid = (ctx, offsetX, offsetY, width, height, gridSize = 50) => {
     ctx.strokeStyle = "#ccc"
     ctx.lineWidth = 1
+
+    for (x = 0; x <= width; x += gridSize){
+        
+    }
 }
