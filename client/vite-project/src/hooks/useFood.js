7c459cef0b4dec34f76a202502cc59235d8ab546
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-07-05 15:59:36 +0300

    foodRef
    We use useRef to store food without triggering re-renders, which improves performance during the game

--- client/vite-project/src/hooks/useFood.js
+++ client/vite-project/src/hooks/useFood.js
@@ -2,6 +2,7 @@ import { useRef, useEffect } from 'react';
 import { createFood } from '../utils/gameUtils';
 
 const useFood = (mapWidth, mapHeight, initialCount = 200, respawnCount = 10, respawnInterval = 4000) => {
-    
+    const foodRef = useRef(createFood(initialCount, mapWidth, mapHeight));
 
+    
 }
